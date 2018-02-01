64ac9550994c2e83ed68aedcedb90a397e75f587
Katherine Worley <kdesign216@yahoo.com>
2018-02-01 14:57:16 -0500

    created controller for items and updated methods in controller

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,18 @@
+class TodoItemsController < ApplicationController
+	def create
+		@todo_list = TodoList.find(params[:todo_list_id])
+		@todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+	end
+
+	private
+
+	def set_todo_list
+		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def todo_items_params
+		params[:todo_item].permit(:content)
+	end
+
+
+end
