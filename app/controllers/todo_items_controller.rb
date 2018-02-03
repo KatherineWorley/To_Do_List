acf4b6111b4a793fb62fd5fabbe8a128fbff0bf7
Katherine Worley <kdesign216@yahoo.com>
2018-02-03 11:45:53 -0500

    added the ability to destroy the item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,8 +5,17 @@ class TodoItemsController < ApplicationController
 	def create
 		@todo_list = TodoList.find(params[:todo_list_id])
 		@todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+	end
 
-		
+	def destroy
+		@todo_item = @todo_list.todo_items.find(params[:id])
+		if @todo_item.destroy
+			flash[:success] = "Todo List item was deleted"
+		else
+			flash[:error] = "todo List item could not be deleted"
+		end
+
+		redirect_to @todo_list
 	end
 
 
