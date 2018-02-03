ab41cbf64a5b54b06c2e90afe9cf97afd75c3f68
Katherine Worley <kdesign216@yahoo.com>
2018-02-03 11:57:00 -0500

    marking todo item as complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create]
 
 
 	def create
@@ -8,7 +9,6 @@ class TodoItemsController < ApplicationController
 	end
 
 	def destroy
-		@todo_item = @todo_list.todo_items.find(params[:id])
 		if @todo_item.destroy
 			flash[:success] = "Todo List item was deleted"
 		else
@@ -18,11 +18,20 @@ class TodoItemsController < ApplicationController
 		redirect_to @todo_list
 	end
 
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "Todo item competed"
+	end
+
 
 	private
 
 	def set_todo_list
 		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
 	end
 
 	def todo_items_params
